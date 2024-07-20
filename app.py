f5727012ce4000469a8c373c42eab233a5536feb
ZiadMostafa1 <ziad.zero1245@gmail.com>
2024-07-21 02:21:01 +0300

    app edit

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
 
     # set page defult mode to light mode
 
-    
+
     st.set_page_config(page_title='Diamond Price Prediction App', page_icon='💎')
     st.title('Diamond Price Prediction App 💎')
     
@@ -25,8 +25,8 @@ def main():
     cut = st.selectbox('Cut (Quality of the cut)', ['Ideal', 'Premium', 'Very Good', 'Good', 'Fair'], index=1)
     color = st.selectbox('Color (Color grade of the diamond)', ['D', 'E', 'F', 'G', 'H', 'I', 'J'], index=3)  
     clarity = st.selectbox('Clarity (Measure of how clear the diamond is)', ['IF', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'I1'], index=6)  
-    depth = st.number_input('Depth (Depth %, total depth relative to diameter)', value=60.9)
-    table = st.number_input('Table (Table %, width of the top of the diamond relative to widest point)', value=50)
+    depth = st.number_input('Depth (Depth %, total depth relative to diameter)', value=60.9, format="%.2f")
+    table = st.number_input('Table (Table %, width of the top of the diamond relative to widest point)', value=50.0, format="%.2f")
     x = st.number_input('X (Length in mm)', value=4.00, format="%.2f")
     y = st.number_input('Y (Width in mm)', value=8.00, format="%.2f")
     z = st.number_input('Z (Depth in mm)', value=7.00, format="%.2f")
